7b23105afcd535d2fe215ef19a61394d481550f6
Connor Watson <cwatson@tactile-engineering.com>
2024-04-17 20:47:21 +0000

    upgrades

---
+++ pages/Tablet Tracking/jsobjects/FetchTabletData/FetchTabletData.js
@@ -0,0 +1,9 @@
+export default {
+	TabletDataFetch: async() =>  {
+		const TabletData = QueryTop5kTablets.data
+		function FilterByStatus() {
+			return TabletData.filter(TabletData(S) => "NEW" )}
+		
+		return FilterByStatus()
+	}
+}
---
+++ pages/Tablet Tracking/jsobjects/FetchTabletDataCopy/FetchTabletDataCopy.js
@@ -0,0 +1,8 @@
+export default {
+	TabletNumFetch: async() =>  {
+		const TabletNums = 
+
+		return TabletNums
+		Response.sort()
+	}
+}
