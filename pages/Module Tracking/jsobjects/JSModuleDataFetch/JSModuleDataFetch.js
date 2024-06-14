8687b76ee9449338cf8489196c0a9c922a5b6ba2
Connor Watson <cwatson@tactile-engineering.com>
2024-06-14 04:15:55 +0000

    Created TimeKeeperPackage Initial module tracker page

--- pages/Module Tracking/jsobjects/JSModuleDataFetch/JSModuleDataFetch.js
+++ pages/Module Tracking/jsobjects/JSModuleDataFetch/JSModuleDataFetch.js
@@ -3,6 +3,6 @@ export default {
 		const ModuleData = SelectModuleList.data
 
 		return ModuleData
-		Response.sort()
+
 	}
 }
---
+++ pages/Module Tracking/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,6 @@
+export default {
+	setInterval(() => moment.now(), 1000 'CurrentTime')
+	}
+
+}
+
